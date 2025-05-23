cf89f4aefce6cc0eba3ff55e8ecee24a9811335f
김승찬 <tmdcks801>
2025-05-23 09:54:08 +0900

    관리자 권한 생성시 수정

--- src/main/java/com/sprint/mission/discodeit/component/Init.java
+++ src/main/java/com/sprint/mission/discodeit/component/Init.java
@@ -1,9 +1,14 @@
 package com.sprint.mission.discodeit.component;
 
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
 import com.sprint.mission.discodeit.eenum.Role;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
 import jakarta.annotation.PostConstruct;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -11,6 +16,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class Init {
+  private final UserService userService;
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
 
@@ -19,17 +25,10 @@ public class Init {
     String adminUsername = "admin";
     String adminEmail = "admin@admin.com";
 
+    UserCreateRequest userCreateRequest=new UserCreateRequest(adminUsername,adminEmail,"tmdcks1234");
     boolean exists = userRepository.existsByUsername(adminUsername);
     if (exists) return;
-
-    User admin = new User(
-        adminUsername,
-        adminEmail,
-        passwordEncoder.encode("tmdcks1234"),
-        null
-    );
-    admin.setRole(Role.ROLE_ADMIN);
-
-    userRepository.save(admin);
+    UserDto user=userService.create(userCreateRequest,null);
+    userService.updateUserRole(user.id(),Role.ROLE_ADMIN);
   }
 }
